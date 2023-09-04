cf82d42f322e2cc7e300cdba3a6c0f27c581b8e2
Afonso Bernardes <84087794+AfonsoBernardes@users.noreply.github.com>
2023-09-04 18:26:51 +0000

    ToDo basic layout.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,19 +6,23 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid vh-100 bg-dark d-flex justify-content-center">
+			<div className="todo-list container bg-white m-auto p-5 w-50">
+
+				<h1 class="todo-header text-center">To do List</h1>
+				<input id="addToDo" type="text" placeholder="Add to do here" />
+				<ul className="my-3">
+					<li>
+						<span><i class="fa fa-trash"></i></span> Eat
+					</li>
+					<li>
+						<span><i class="fa fa-trash"></i></span> Drink
+					</li>
+					<li>
+						<span><i class="fa fa-trash"></i></span> Sleep
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
